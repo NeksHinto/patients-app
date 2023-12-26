ced70271960d16b56d05673049e2a0a689432abe
NeksHinto <nhinojo@itba.edu.ar>
2023-12-26 17:45:12 -0300

    [NEW] HomePage

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PatientList from '../components/PatientList/PatientList';
+
+const Home: React.FC = () => {
+	return (
+		<div>
+			<PatientList />
+		</div>
+	);
+};
+
+export default Home;
