1d622d6f1e580b4706ec1c3283ddfdf57cb980a1
NeksHinto <nhinojo@itba.edu.ar>
2023-12-27 05:16:11 -0300

    [HomePage] Insert Context Provider wrapper

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PatientList from '../components/PatientList/PatientList';
+import { PatientProvider } from '../contexts/app-context';
 
 const Home: React.FC = () => {
+
 	return (
-		<div>
+		<PatientProvider>
 			<PatientList />
-		</div>
+		</PatientProvider>
 	);
 };
 
