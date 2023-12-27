56b58260d0a2361502c5d09b91a6f11d677c7191
NeksHinto <nhinojo@itba.edu.ar>
2023-12-27 10:49:52 -0300

    Remove unused files

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProviderWrapper } from './contexts/Theme/theme-context';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProviderWrapper>
+      <App />
+    </ThemeProviderWrapper>
   </React.StrictMode>
 );
 
